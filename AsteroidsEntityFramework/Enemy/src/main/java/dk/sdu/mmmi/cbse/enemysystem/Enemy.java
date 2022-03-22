1e17ab16dd42a3638d4540f7b7d0996393097512
Joachim Baumann <jobau19@student.sdu.dk>
2022-03-22 07:40:10 +0100

    added enemy module and glued to game.

--- AsteroidsEntityFramework/Enemy/src/main/java/dk/sdu/mmmi/cbse/enemysystem/Enemy.java
+++ AsteroidsEntityFramework/Enemy/src/main/java/dk/sdu/mmmi/cbse/enemysystem/Enemy.java
@@ -4,9 +4,9 @@ import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.entityparts.MovingPart;
 
 /**
- *
  * @author corfixen
  */
-public class Enemy extends Entity {
+public class
+Enemy extends Entity {
 
 }
